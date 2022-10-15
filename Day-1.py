fc9d8e8eea41ba7f16959a5cfc46e2691e34773c
Keerthana Kirshnamoorthy <107944137+KeerthanaKrishnamoorthy@users.noreply.github.com>
2022-10-15 12:11:54 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,22 @@
+#hello world
+print("Hello,World")
+
+#Data types
+print(2+3)    #addition
+print(2-1)    #subtraction
+print(2*5)    #multiplication
+print(10/2)   #division
+print(10**2)  #exponential
+print(2%6)    #modulus
+print(5//6)   #floor division
+
+# To check the data types
+print(type(10))                                  #int
+print(type(10.2))                                #float
+print(type(10+4j))                               #complex
+print(type("elonmusk"))                          #string
+print(type([1,5,6]))                             #list
+print(type({"name": "kiki", "age": 24}))         #dictionary
+print(type(("apple",)))                          #tuple
+print(type({52,"kiki",True}))                    #set
+
