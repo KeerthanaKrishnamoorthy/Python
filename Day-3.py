0833799923120514a2ed388ae065c5217d8d2324
Keerthana Kirshnamoorthy <107944137+KeerthanaKrishnamoorthy@users.noreply.github.com>
2022-10-17 15:26:05 +0530

    Update Day-3.py

--- Day-3.py
+++ Day-3.py
@@ -121,6 +121,13 @@ a = "python"
 print(float(6))
 print(str("python"))
 
+#pyhton script
+print(1, 1 ,1 ,1, 1)
+print(2, 1 ,2 ,4 ,8)
+print(3, 1 ,3, 9 ,27)
+print(4, 1 ,4, 16, 64)
+print(5 ,1, 5, 25, 125)
+
 
 
 
